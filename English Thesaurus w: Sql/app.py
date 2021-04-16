8b08940fce2073b43f2f6b4fc768e2bda6309ef0
Kevin Floyd <kfloydy@gmail.com>
2021-04-16 18:31:45 -0500

    Update app.py

--- English Thesaurus w: Sql/app.py
+++ English Thesaurus w: Sql/app.py
@@ -6,3 +6,10 @@ con = mysql.connector.connect(
     host="108.167.140.122",
     database="ardit700_pm1database"
 )
+
+cursor = con.cursor()
+
+query = cursor.execute("SELECT * FROM Dictionary")
+results = cursor.fetchall()
+
+print(results)
